2152a6f4c82bb4e16f91a055ab1bf9944fdcadf4
Phil Evans <email@philevans.co>
2019-06-05 11:29:20 -0400

    new image block template

--- h7-parent/blocks/image.php
+++ h7-parent/blocks/image.php
@@ -1,7 +1,44 @@
 <?php
   
-  $img_URL = wp_get_attachment_image_url( get_sub_field ( 'image' ), 'full' );
+  $img_ID = get_sub_field ( 'image' );
   
+  if ( $img_ID != '' ) {
+    
+    $img_post = get_post ( $img_ID );
+    
+    $img_URL = wp_get_attachment_image_url ( $img_ID, 'full' );
+    $img_alt = get_post_meta ( $img_ID, '_wp_attachment_image_alt', true );
+    
+    $img_title = get_the_title ( $img_ID );
+    $img_caption = get_the_excerpt ( $img_ID );
+    $img_content = $img_post->post_content;
+    
 ?>
 
-<img src="<?php echo $img_URL; ?>">
+<div class="row">
+  <div class="col-10 offset-1">
+    <h4><?php echo $img_title; ?></h4>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-10 offset-1 image">
+    <img src="<?php echo $img_URL; ?>" alt="<?php echo $img_alt; ?>">
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-10 offset-1 image-caption">
+    <?php 
+      
+      echo apply_filters ( 'the_content', $img_content );
+      
+    ?>
+  </div>
+</div>
+
+<?php
+  
+  }
+  
+?>

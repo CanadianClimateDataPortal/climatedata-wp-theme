f89c7104745a20eb85c2e5f7ad7c157b241622c5
Louis-David Perron <100434291+perronld@users.noreply.github.com>
2020-02-11 11:25:09 -0500

    Changed e-mail destination to training download form

--- climate-data-ca/resources/ajax/training-submit.php
+++ climate-data-ca/resources/ajax/training-submit.php
@@ -21,36 +21,30 @@ if ($securimage->check($_GET['terms-captcha_code']) == false) {
   $form_data = $_GET;
   
   //$to = get_option ( 'admin_email' );
-  $to = 'phil@makeitoperativ.com';
-  $subject = get_bloginfo ( 'title' ) . ': Feedback form submission';
+  $to = 'support-backup+training@climatedata.ca';
+  $subject = get_bloginfo ( 'title' ) . __(': Training download','cdc-feedback');
   
   $headers = array ( 
-    'Content-Type: text/html; charset=UTF-8'
+    'Content-Type: text/html; charset=UTF-8',
+    "From: {$form_data['name']} <{$form_data['email']}>"
   );
-  
-  $body = '<h2>You’ve received a new form submission.</h2>';
-  
-  $body .= '<p><span style="display: inline-block; width: 150px; font-weight: bold; vertical-align: top;">Name</span><span style="display: inline-block; vertical-align: top;">' . $form_data['fullname'] . '</span></p>';
+  $body = '<p>The following invididual aggreed the terms of use and downloaded the training presentation</p>';
+  $body .= '<p><span style="display: inline-block; width: 150px; font-weight: bold; vertical-align: top;">Name</span><span style="display: inline-block; vertical-align: top;">' . $form_data['name'] . '</span></p>';
   
   $body .= '<p><span style="display: inline-block; width: 150px; font-weight: bold; vertical-align: top;">Email</span><span style="display: inline-block; vertical-align: top;"><a href="mailto:' . $form_data['email'] . '">' . $form_data['email'] . '</a></span></p>';
-  
-  $body .= '<p><span style="display: inline-block; width: 150px; font-weight: bold; vertical-align: top;">Organization</span><span style="display: inline-block; vertical-align: top;">' . $form_data['organization'] . '</span></p>';
-  
-  $body .= '<p><span style="display: inline-block; width: 150px; font-weight: bold; vertical-align: top;">Type</span><span style="display: inline-block; vertical-align: top;">' . $form_data['feedback-type'] . '</span></p>';
-  
-  $body .= '<p style="font-weight: bold;">Message</p><p><pre>' . $form_data['feedback'] . '</pre></p>';
   
   $body .= '<hr>';
   
   $body .= '<p style="font-size: 80%">This message was sent at ' . current_time ('H:i:s' ) . ' on ' . current_time ( 'F j, Y' ) . '</p>';
   
-//   wp_mail ( $to, $subject, $body, $headers );
+  wp_mail ( $to, $subject, $body, $headers );
+  // wp_mail ( "info.cccs-ccsc@canada.ca", $subject, $body, $headers );
   
   $response['message'] = 'success';
   
   // find the attached PPT
   
-  $post_attachments = get_attached_media ( '', $_GET['page_ID'] );
+  // $post_attachments = get_attached_media ( '', $_GET['page_ID'] );
   
   $response['url'] = wp_get_attachment_url ( get_field ( 'training_presentation', $_GET['page_ID'] ) );
   

bd057f223480c4752aaa2dacdbee9308d7a1cf30
Phil Evans <email@philevans.co>
2021-12-08 10:53:30 -0400

    added ID attributes to glossary terms so they can be #hash linked to from other pages

--- climate-data-ca/tpl-glossary.php
+++ climate-data-ca/tpl-glossary.php
@@ -1,41 +1,41 @@
 <?php
-  
+
   /*
-    
+
     Template Name: Glossary
-    
+
   */
-  
+
   //
   // ENQUEUE
   //
-  
+
   function tpl_enqueue() {
-    
+
     wp_enqueue_script ( 'listnav' );
-    
+
   }
-  
+
   add_action ( 'wp_enqueue_scripts', 'tpl_enqueue' );
-  
+
   //
   // TEMPLATE
   //
 
   get_header();
-  
+
   if ( have_posts() ) : while ( have_posts() ) : the_post();
-  
+
 ?>
 
 <main id="glossary-content">
-  
+
   <?php
-    
+
     include ( locate_template ( 'template/hero/hero.php' ) );
-    
+
   ?>
-  
+
   <nav class="navbar navbar-expand navbar-light bg-light">
     <aside id="glossary-listnav-nav" class="collapse navbar-collapse">
 <!--
@@ -45,61 +45,61 @@
 -->
     </aside>
   </nav>
-  
+
   <section id="glossary" class="page-section">
 
       <?php
-        
+
         $glossary_query = new WP_Query ( array (
           'post_type' => 'definition',
           'posts_per_page' => -1,
           'orderby' => 'title',
           'order' => 'asc'
         ) );
-        
-        if ( $glossary_query->have_posts() ) : 
-        
+
+        if ( $glossary_query->have_posts() ) :
+
       ?>
-      
+
       <div class="container-fluid">
         <ul id="glossary-listnav">
-        
+
           <?php
-            
+
             while ( $glossary_query->have_posts() ) : $glossary_query->the_post();
-          
+
           ?>
-          
-          <li class="dl-item row">
+
+          <li id="def-<?php echo get_the_id(); ?>" class="dl-item row">
             <span class="dt col-10 offset-1 col-sm-2 col-lg-3"><?php the_title(); ?></span>
             <span class="dd col-10 offset-1 col-sm-8 offset-sm-0 col-lg-6"><?php the_content(); ?></span>
           </li>
-        
+
           <?php
-          
+
             endwhile;
-          
+
           ?>
-      
+
         </ul>
       </div>
-      
+
       <?php
-        
+
         endif;
-        
+
         wp_reset_postdata();
-        
+
       ?>
 
   </section>
-  
+
 </main>
 
 <?php
-  
+
   endwhile; endif;
-  
+
   get_footer();
-  
+
 ?>

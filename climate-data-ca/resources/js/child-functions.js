8d88427d2df08dcd54c02886967965e53a4beff1
Louis-David Perron <100434291+perronld@users.noreply.github.com>
2022-12-05 15:15:26 -0500

    CLIM-282 Added FR translation for JS strings

--- climate-data-ca/resources/js/child-functions.js
+++ climate-data-ca/resources/js/child-functions.js
@@ -15,6 +15,12 @@ var chart_labels, legend_labels, l10n_labels;
 
 var l10n_table = {
     "fr": {
+
+        // share widget
+        "Copied to clipboard": "Copié dans le presse-papier",
+        "Error" : "Erreur",
+        "Copy link": "Copier le lien",
+
         "With the current frequency and format setting, the maximum number of grid boxes that can be selected per request is {0}":
             "Avec les paramètres actuels de fréquence et de format de donnée, le nombre maximal de points de grille par requête est de {0}",
         "Around {0} grid boxes selected" : "Environ {0} points de grille sélectionnés"

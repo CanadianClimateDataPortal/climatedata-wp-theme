0c12e50e9b782f0ca394f56c0e8e1fae4384334a
Azur Handan <handanaz@ppm-haaz1.local>
2023-02-23 18:06:10 -0500

    Iframe onload checker if timeout

---
+++ climate-data-ca/resources/js/iframe-functions.js
@@ -0,0 +1,26 @@
+(function ($) {
+    $(function () {
+
+        const timeout_timer = document.getElementById("i_frame").getAttribute('timeout_timer')
+
+        if (timeout_timer > 0) {
+            // Timeout of 10 sec to healthcheck on the iframe.
+            let timeoutID = setTimeout(errorMessageIframe, timeout_timer * 1000);
+
+            // Event listener on the window wich will reveive a message from the iframe.
+            $(window).on("message onmessage", function(e) {
+                console.log("Message from iframe:" + e.originalEvent.data)
+                clearInterval(timeoutID);
+            });
+        }
+
+
+        // Function overiding the "srcDoc" attribute from the iframe to an Error Message
+        function errorMessageIframe() {
+            console.log("Iframe Failed.")
+            let iframe = document.getElementById('i_frame');
+            iframe.srcdoc = "This iframe failed to load after "+timeout_timer+" sec. <br>Check the URL : " +iframe.src + "<br> Also make sure the iframe url application send a message onmesage.";
+        }
+       
+    })
+ })(jQuery);
---
+++ climate-data-ca/tpl-iframe.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+  Template Name: IFrame
+*/
+
+//
+// ENQUEUE
+//
+
+function tpl_enqueue() {
+
+  wp_enqueue_script ( 'iframe-functions' );
+  
+}
+add_action ( 'wp_enqueue_scripts', 'tpl_enqueue' );
+
+get_header();
+if (have_posts()) : while (have_posts()) : the_post();
+?>
+
+<main id="iframe-content">
+
+
+<?php
+
+  include (locate_template('template/hero/hero.php'));
+  
+?>
+<section id="iframe-section" class="page-section bg-white" >
+  <div class="iframe-container">
+    <iframe src=<?php echo get_field("url") ?> id="i_frame" title="iframe" allow="fullscreen" timeout_timer=<?php echo get_field("timeout_timer") ?> ></iframe>
+  </div>
+</section>
+
+</main>
+
+
+<?php
+endwhile; endif;
+
+get_footer();
+
+?>

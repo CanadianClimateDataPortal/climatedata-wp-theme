0a89dc4a451ca97cc3e32f581b19fca3b602f8f7
Phil <phil-evans@users.noreply.github.com>
2023-11-27 22:02:32 -0400

    misc markup changes

--- fw-child/template/download/control-bar.php
+++ fw-child/template/download/control-bar.php
@@ -1,4 +1,4 @@
-<div id="control-bar" class="control-bar tab-drawer-tabs-container">
+<div id="control-bar" class="control-bar tab-drawer-tabs-container d-flex flex-column align-items-center">
 	<?php 
 	
 		// LOGO
@@ -7,26 +7,42 @@
 		
 	?>
 	
-	<a href="#menu" id="menu-trigger" class="d-block p-1"><i class="fas fa-align-left text-white"></i></a>
+	<a href="#menu" id="menu-trigger" class="d-block p-2"><i class="fas fa-align-left text-white"></i></a>
 	
-	<div id="control-bar-tabs" class="tab-drawer-tabs">
+	<div id="control-bar-tabs" class="tab-drawer-tabs w-100">
 		<a href="#data" class="tab-drawer-trigger">
-			<span class="cdc-icon"></span>
+			<span class="cdc-icon"><?php
+				
+					echo file_get_contents ( locate_template ( 'resources/img/icon-data.svg' ) );
+					
+				?></span>
 			<span><?php _e ( 'Data', 'cdc' ); ?></span>
 		</a>
 		
 		<a href="#area" class="tab-drawer-trigger">
-			<span class="cdc-icon"></span>
+			<span class="cdc-icon"><?php
+				
+					echo file_get_contents ( locate_template ( 'resources/img/icon-location.svg' ) );
+					
+				?></span>
 			<span><?php _e ( 'Area', 'cdc' ); ?></span>
 		</a>
 		
 		<a href="#details" class="tab-drawer-trigger">
-			<span class="cdc-icon"></span>
+			<span class="cdc-icon"><?php
+				
+					echo file_get_contents ( locate_template ( 'resources/img/icon-display.svg' ) );
+					
+				?></span>
 			<span><?php _e ( 'Details', 'cdc' ); ?></span>
 		</a>
 		
 		<a href="#submit" class="tab-drawer-trigger">
-			<span class="cdc-icon"></span>
+			<span class="cdc-icon"><?php
+				
+					echo file_get_contents ( locate_template ( 'resources/img/icon-download.svg' ) );
+					
+				?></span>
 			<span><?php _e ( 'Submit', 'cdc' ); ?></span>
 		</a>
 	</div>

2ac387bf5219e46245a0d6a9c1acb18bced8a8fd
Phil <phil-evans@users.noreply.github.com>
2023-12-11 12:28:20 -0400

    post content migration bug fixes

--- fw-child/page-convert-tags.php
+++ fw-child/page-convert-tags.php
@@ -17,8 +17,12 @@ foreach ( $post_types as $post_type ) {
 	$pt_query = new WP_Query ( array (
 		'post_type' => $post_type,
 		'post_status' => 'publish',
-		'posts_per_page' => 1,
+		'posts_per_page' => -1,
 	));
+	
+	// $pt_query = new WP_Query ( array (
+	// 	'p' => 11293
+	// ));
 	
 	while ( $pt_query->have_posts() ) {
 		
@@ -33,11 +37,16 @@ foreach ( $post_types as $post_type ) {
 		$en_ID = get_the_ID();
 		$fr_ID = apply_filters ( 'wpml_object_id', get_the_ID(), $post_type, false, 'fr' );
 		
+		echo 'en: ' . $en_ID . '<br>';
+		echo 'fr: ' . $fr_ID . '<br>';
+		
 		// 3. insert 'standard page' builder layout
 		
 		update_post_meta ( $en_ID, 'builder', '{ "type": "page", "inputs": { "id": "auto" }, "post_id": "' . $en_ID . '", "key": "' . $en_ID . '", "children": [ { "type": "template", "inputs": { "source": "include", "post_id": "", "path": "header-fixed.php", "id": "auto", "class": [ "" ] }, "key": "' . $en_ID . '-1" }, { "classes": [ "fw-element", "fw-template" ], "type": "template", "inputs": { "source": "post", "post_id": "11314", "path": "", "id": "auto", "class": [ "" ] }, "key": "' . $en_ID . '-2" }, { "type": "template", "inputs": { "source": "post", "post_id": "11323", "path": "", "id": "auto", "class": [ "" ] }, "key": "' . $en_ID . '-3" }, { "classes": [ "fw-element", "fw-template" ], "type": "template", "inputs": { "source": "post", "post_id": "11354", "path": "", "id": "auto", "class": [ "" ] }, "key": "' . $en_ID . '-4" }, { "type": "section", "key": "' . $en_ID . '-5", "inputs": { "id": "auto", "class": [] }, "children": [ { "type": "template", "inputs": { "source": "include", "post_id": null, "path": "old-post-loop.php", "output": null, "id": "auto", "class": [ "" ] }, "key": "' . $en_ID . '-5-1" } ] }, { "classes": [ "fw-element", "fw-template" ], "type": "template", "inputs": { "source": "post", "post_id": "11315", "path": "", "id": "auto", "class": [ "" ] }, "key": "' . $en_ID . '-6" } ] }' );
 		
 		echo 'updated builder layout<br>';
+		
+		update_post_meta ( $en_ID, 'hero_title', get_the_title() );
 		
 		// 4. get the HTML
 		// using a modified loop of the OLD system
@@ -50,7 +59,7 @@ foreach ( $post_types as $post_type ) {
 		
 		$output = ob_get_clean();
 		
-		ob_end_flush();
+		// ob_end_flush();
 		
 		// 5. add it to 'old_output_en'
 		
@@ -58,13 +67,16 @@ foreach ( $post_types as $post_type ) {
 		
 		echo 'populated english field<br>';
 		
+		// echo $output;
+		
 		// 6. repeat for FR
 		
 		if ( $fr_ID != '' ) {
 			
 			// title
-			update_post_meta ( $en_ID, 'title_fr', get_the_slug ( $fr_ID ) );
-	
+			update_post_meta ( $en_ID, 'title_fr', get_the_title ( $fr_ID ) );
+			update_post_meta ( $en_ID, 'hero_title_fr', get_the_title ( $fr_ID ) );
+		
 			// slug/path
 			update_post_meta ( $en_ID, 'slug_fr', get_the_slug ( $fr_ID ) );
 			update_post_meta ( $en_ID, 'path_fr', get_the_slug ( $fr_ID ) );
@@ -108,8 +120,6 @@ foreach ( $post_types as $post_type ) {
 		
 		// not really necessary
 		wp_reset_postdata();
-		
-		// echo $output;
 		
 		echo '<hr>';
 		

c09df41709dec621d87379ff46b3c21b33416961
Phil <phil-evans@users.noreply.github.com>
2023-12-14 12:17:13 -0400

    [fw] update 'custom field value' block

--- framework/resources/functions/builder/front-end/block/content/field.php
+++ framework/resources/functions/builder/front-end/block/content/field.php
@@ -21,10 +21,30 @@
 		$field_output = wptexturize ( $field_output );
 	}
 
+	if ( $field_output != '' ) {
+
 ?>
 
 <<?php echo $element['inputs']['display']; ?>><?php
-	
+
+	echo $element['inputs']['prepend'];
 	echo $field_output;
+	echo $element['inputs']['append'];
 	
 ?></<?php echo $element['inputs']['display']; ?>>
+
+<?php
+
+	} elseif ( is_user_logged_in() ) {
+		
+		// show warning if logged in
+		
+?>
+
+<div class="alert alert-warning fw-builder-alert">Custom field <code><?php echo  $key; ?></code> is empty. Click <span class="py-1 px-2 mx-1 text-bg-light"><i class="fas fa-pencil-alt me-2"></i>Edit in Wordpress</span> to add a value.</div>
+
+<?php
+
+	}
+	
+?>

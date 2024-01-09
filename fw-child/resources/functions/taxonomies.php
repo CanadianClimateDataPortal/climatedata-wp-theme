287c1b3b096127c9b2086f1a2d8c90a99052f7fb
Phil <phil-evans@users.noreply.github.com>
2024-01-09 11:48:25 -0400

    add 'technical level' taxonomy

--- fw-child/resources/functions/taxonomies.php
+++ fw-child/resources/functions/taxonomies.php
@@ -47,7 +47,6 @@ add_action( 'init', 'taxonomy_resourcecat', 0 );
 
 // topic
 
-// Register Custom Taxonomy
 function taxonomy_topic() {
 
 	$labels = array(
@@ -86,3 +85,43 @@ function taxonomy_topic() {
 
 }
 add_action( 'init', 'taxonomy_topic', 0 );
+
+// technical level
+
+function taxonomy_techlevel() {
+
+	$labels = array(
+		'name'                       => _x( 'Technical Levels', 'Taxonomy General Name', 'cdc-taxonomies' ),
+		'singular_name'              => _x( 'Technical Level', 'Taxonomy Singular Name', 'cdc-taxonomies' ),
+		'menu_name'                  => __( 'Technical Levels', 'cdc-taxonomies' ),
+		'all_items'                  => __( 'All Levels', 'cdc-taxonomies' ),
+		'parent_item'                => __( 'Parent Level', 'cdc-taxonomies' ),
+		'parent_item_colon'          => __( 'Parent Level:', 'cdc-taxonomies' ),
+		'new_item_name'              => __( 'New Level Name', 'cdc-taxonomies' ),
+		'add_new_item'               => __( 'Add New Level', 'cdc-taxonomies' ),
+		'edit_item'                  => __( 'Edit Level', 'cdc-taxonomies' ),
+		'update_item'                => __( 'Update Level', 'cdc-taxonomies' ),
+		'view_item'                  => __( 'View Level', 'cdc-taxonomies' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'cdc-taxonomies' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'cdc-taxonomies' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'cdc-taxonomies' ),
+		'popular_items'              => __( 'Popular Items', 'cdc-taxonomies' ),
+		'search_items'               => __( 'Search Items', 'cdc-taxonomies' ),
+		'not_found'                  => __( 'Not Found', 'cdc-taxonomies' ),
+		'no_terms'                   => __( 'No items', 'cdc-taxonomies' ),
+		'items_list'                 => __( 'Items list', 'cdc-taxonomies' ),
+		'items_list_navigation'      => __( 'Items list navigation', 'cdc-taxonomies' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => false,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => false,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'tech-level', array( 'resource' ), $args );
+
+}
+add_action( 'init', 'taxonomy_techlevel', 0 );

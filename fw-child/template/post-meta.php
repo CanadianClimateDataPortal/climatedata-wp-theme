d8744058acb0ae91e84fdcccbcf4b7d72c4efd40
Phil <phil-evans@users.noreply.github.com>
2024-01-18 11:50:07 -0400

    styling for news post hero meta area

--- fw-child/template/post-meta.php
+++ fw-child/template/post-meta.php
@@ -1,15 +1,19 @@
-<h6><?php _e ( 'Date', 'cdc' ); ?></h6>
-
-<?php 
-
-	the_time ( 'F j, Y' );
-
-?>
-
-<h6><?php _e ( 'Author', 'cdc' ); ?></h6>
-
-<?php
-
-	the_field ( 'post_author' );
+<div class="mb-2">
+	<h6 class="all-caps text-gray-300"><?php _e ( 'Date', 'cdc' ); ?></h6>
 	
-?>
+	<?php 
+	
+		the_time ( 'F j, Y' );
+	
+	?>
+</div>
+
+<div class="mb-2">	
+	<h6 class="all-caps text-gray-300"><?php _e ( 'Author', 'cdc' ); ?></h6>
+	
+	<?php
+	
+		the_field ( 'post_author' );
+		
+	?>
+</div>

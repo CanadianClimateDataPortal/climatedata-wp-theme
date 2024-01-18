23f8299295ae0d7883207fd669ab42f0279fb93b
Phil <phil-evans@users.noreply.github.com>
2024-01-18 12:56:04 -0400

    add tag list to post meta

--- fw-child/template/post-meta.php
+++ fw-child/template/post-meta.php
@@ -8,7 +8,7 @@
 	?>
 </div>
 
-<div class="mb-2">	
+<div class="mb-2">
 	<h6 class="all-caps text-gray-300"><?php _e ( 'Author', 'cdc' ); ?></h6>
 	
 	<?php
@@ -17,3 +17,33 @@
 		
 	?>
 </div>
+
+<?php
+	
+	$post_tags = get_the_terms ( get_the_ID(), 'post_tag' );
+	
+	if ( !empty ( $post_tags ) ) {
+		
+?>
+
+<div class="mb-2">
+	<h6 class="all-caps text-gray-300"><?php _e ( 'Topics', 'cdc' ); ?></h6>
+	
+	<p><?php
+	
+			$i = 0;
+			
+			foreach ( $post_tags as $tag ) {
+				if ( $i != 0 ) echo ', ';
+				echo '<a href="/news/">' . $tag->name . '</a>';
+				$i++;
+			}
+			
+	?></p>
+</div>
+	
+<?php
+	
+}
+
+?>

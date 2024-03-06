77ad3332135a1208158ca3ab7082c3655a137431
Phil <phil-evans@users.noreply.github.com>
2024-03-05 20:32:26 -0400

    [fw] misc adjustments

--- framework/resources/js/global-functions.js
+++ framework/resources/js/global-functions.js
@@ -30,7 +30,7 @@ var theme_dir = current_script.replace('/resources/js/' + filename, '') + '/'
         
         let this_swiper = $(this).find('> .swiper'),
             this_settings = JSON.parse($(this).attr('data-swiper-settings'))
-        
+            
         if (
           $('body').hasClass('fw-builder') && 
           this_settings.hasOwnProperty('autoplay')

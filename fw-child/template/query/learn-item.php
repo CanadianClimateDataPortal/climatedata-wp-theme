022a53a30d34f03fac527108558e61e496650aa7
Phil <phil-evans@users.noreply.github.com>
2024-03-07 21:57:06 -0400

    template additions

--- fw-child/template/query/learn-item.php
+++ fw-child/template/query/learn-item.php
@@ -18,11 +18,35 @@
 	<div class="card-body">
 		<h5 class="card-title item-title"><a href="<?php echo $item['permalink']; ?>" class="text-secondary stretched-link"><?php echo $item['title']; ?></a></h5>
 		
-		<p><?php
+		<div class="mb-3">
+			<?php
+			
+				echo apply_filters ( 'the_content', custom_excerpt ( 20, $item['id'] ) );
+			
+			?>
+		</div>
 		
-			the_field ( 'asset_type', $item['id'] );
-		
-		?></p>
+		<div class="row row-cols-2">
+			<div class="col">
+				<h6 class="all-caps text-secondary"><?php _e ( 'Format', 'cdc' ); ?></h6>
+	
+				<p class="mb-0"><?php
+				
+					the_field ( 'asset_type', $item['id'] );
+				
+				?></p>
+			</div>
+			
+			<div class="col">
+				<h6 class="all-caps text-secondary"><?php _e ( 'Time', 'cdc' ); ?></h6>
+			
+				<p class="mb-0"><?php
+				
+					the_field ( 'asset_time', $item['id'] );
+				
+				?></p>
+			</div>
+		</div>
 		
 	</div>
 </div>

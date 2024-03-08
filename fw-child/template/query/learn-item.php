cb203c1030bb9dee87cf5d7962e6c2607c60371c
Phil <phil-evans@users.noreply.github.com>
2024-03-08 13:36:44 -0400

    query item styling

--- fw-child/template/query/learn-item.php
+++ fw-child/template/query/learn-item.php
@@ -30,19 +30,48 @@
 			<div class="col">
 				<h6 class="all-caps text-secondary"><?php _e ( 'Format', 'cdc' ); ?></h6>
 	
-				<p class="mb-0"><?php
+				<?php
 				
-					the_field ( 'asset_type', $item['id'] );
+					$format_icon = '';
+					$format_name = get_field ( 'asset_type', $item['id'] );
+					
+					switch ( get_field ( 'asset_type', $item['id'] ) ) {
+						
+						case 'video' :
+							$format_icon = 'fas fa-video';
+							$format_name = __ ( 'Video', 'cdc' );
+							break;
+							
+						case 'audio' :
+							$format_icon = 'fas fa-volume';
+							$format_name = __ ( 'Audio', 'cdc' );
+							break;
+							
+						case 'interactive' :
+							$format_icon = 'far fa-hand-pointer';
+							$format_name = __ ( 'Interactive', 'cdc' );
+							break;
+							
+						case 'article' :
+							$format_icon = 'far fa-newspaper';
+							$format_name = __ ( 'Article', 'cdc' );
+							break;
+							
+					}
+					
+				?>
 				
-				?></p>
+				<p class="mb-0 small text-gray-600">
+					<i class="<?php echo $format_icon; ?> me-2"></i><?php echo $format_name; ?>
+				</p>
 			</div>
 			
 			<div class="col">
 				<h6 class="all-caps text-secondary"><?php _e ( 'Time', 'cdc' ); ?></h6>
 			
-				<p class="mb-0"><?php
+				<p class="mb-0 small text-gray-600"><?php
 				
-					the_field ( 'asset_time', $item['id'] );
+					echo get_field ( 'asset_time', $item['id'] ) . ' minutes';
 				
 				?></p>
 			</div>

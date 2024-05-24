11ab59ba8e6f10365eb13a1279e04379aa8032f8
sebastian-ew <sebastian.gonzalez@evolvingweb.com>
2024-05-24 16:03:56 -0500

    News: Single page: better style post hero (#214)
    Improve the news hero design on the new website to resemble the design of the current website.
    ---------
    
    Co-authored-by: Sebastian Gonzalez <sebastian.gonzalez@evolvingweb.ca>
    Co-authored-by: Mahieddine Abdelkader <mahieddine.abdelkader@evolvingweb.com>

--- fw-child/template/post-meta.php
+++ fw-child/template/post-meta.php
@@ -1,57 +1,59 @@
-<div class="news-meta-item mb-3 mb-lg-4">
-	<h6 class="mb-3 all-caps text-blue-100"><?php _e ( 'Date', 'cdc' ); ?></h6>
-	
-	<?php 
-		
-		the_time ( __( 'F j, Y', 'cdc' ) );
-	
-	?>
-</div>
+<div class="news-meta-block p-3 border border-gray-600">
+	<div class="news-meta-item mb-3 mb-lg-4">
+		<span class="mb-1 all-caps text-blue-100 d-block"><?php _e ( 'Date', 'cdc' ); ?></span>
 
-<div class="news-meta-item mb-3 mb-lg-4">
-	<h6 class="mb-3 all-caps text-blue-100"><?php _e ( 'Author', 'cdc' ); ?></h6>
-	
+		<?php
+
+			the_time ( __( 'F j, Y', 'cdc' ) );
+
+		?>
+	</div>
+
+	<div class="news-meta-item mb-3 mb-lg-4">
+		<span class="mb-1 all-caps text-blue-100 d-block"><?php _e ( 'Author', 'cdc' ); ?></span>
+
+		<?php
+
+			the_field ( 'post_author' );
+
+		?>
+	</div>
+
 	<?php
-	
-		the_field ( 'post_author' );
-		
+
+		$post_tags = get_the_terms ( get_the_ID(), 'post_tag' );
+
+		if ( !empty ( $post_tags ) ) {
+
 	?>
-</div>
 
-<?php
-	
-	$post_tags = get_the_terms ( get_the_ID(), 'post_tag' );
-	
-	if ( !empty ( $post_tags ) ) {
-		
-?>
+	<div class="news-meta-item mb-3 mb-lg-4">
+		<span class="mb-1 all-caps text-blue-100 d-block"><?php _e ( 'Topics', 'cdc' ); ?></span>
 
-<div class="news-meta-item mb-3 mb-lg-4">
-	<h6 class="mb-3 all-caps text-blue-100"><?php _e ( 'Topics', 'cdc' ); ?></h6>
-	
-	<p><?php
-	
-			$i = 0;
-			
-			foreach ( $post_tags as $tag ) {
-				if ( $i != 0 ) echo ', ';
-				echo '<a href="/news/">' . $tag->name . '</a>';
-				$i++;
-			}
-			
-	?></p>
-</div>
-	
-<?php
-	
-}
+		<?php
 
-?>
+				$i = 0;
 
-<div class="news-meta-item mb-3 mb-lg-4">
-	<h6 class="mb-3 all-caps text-blue-100"><?php _e ( 'Share this post', 'cdc' ); ?></h6>
-	
-	<div id="post-share-wrap">
-		<div id="share"></div>
+				foreach ( $post_tags as $tag ) {
+					if ( $i != 0 ) echo ', ';
+					echo '<a href="/news/">' . $tag->name . '</a>';
+					$i++;
+				}
+
+		?>
+	</div>
+
+	<?php
+
+	}
+
+	?>
+
+	<div class="news-meta-item">
+		<span class="mb-1 all-caps text-blue-100 d-block"><?php _e ( 'Share this post', 'cdc' ); ?></span>
+
+		<div id="post-share-wrap">
+			<div id="share"></div>
+		</div>
 	</div>
 </div>

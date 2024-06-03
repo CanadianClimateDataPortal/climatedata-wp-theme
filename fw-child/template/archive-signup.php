58466f751c2e80ff48e5895736058f2d4ac4f9ac
Mahieddine Abdelkader <mahieddine.abdelkader@evolvingweb.com>
2024-06-03 14:31:16 -0400

    refs #65019: Fix typo, make field required

--- fw-child/template/archive-signup.php
+++ fw-child/template/archive-signup.php
@@ -1,18 +1,22 @@
 <!-- Begin Mailchimp Signup Form -->
 <div id="mc_embed_signup" class="p-3 mt-5 mt-xl-0 border border-gray-600">
-	<div class="all-caps text-blue-100 mb-2"><?php _e ( 'Climatedata.ca newsletter', 'cdc' ); ?></div>
+	<div class="all-caps text-blue-100 mb-2"><?php _e( 'Climatedata.ca Newsletter', 'cdc' ); ?></div>
 
-	<p><?php _e ( 'Join our newsletter for periodic updates.', 'cdc' ); ?></p>
+	<p><?php _e( 'Join our newsletter for periodic updates.', 'cdc' ); ?></p>
 
-	<form action="https://climatedata.us1.list-manage.com/subscribe/post?u=c52e1be341cba7905716a66b4&amp;id=727cebaf22" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate pe-3" target="_blank" novalidate>
+	<form action="https://climatedata.us1.list-manage.com/subscribe/post?u=c52e1be341cba7905716a66b4&amp;id=727cebaf22"
+		  method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate pe-3"
+		  target="_blank">
 		<div id="mc_embed_signup_scroll">
 
 			<label for="mce-EMAIL" class="sr-only">Email Address</label>
 
 			<div class="input-group mc-field-group border">
-				<input type="email" value="" name="EMAIL" class="required email form-control border-0" id="mce-EMAIL" placeholder="<?php _e ( 'Your email address', 'cdc' ); ?>">
+				<input type="email" value="" name="EMAIL" class="required email form-control border-0" id="mce-EMAIL"
+					   placeholder="<?php _e( 'Your email address', 'cdc' ); ?>" required>
 
-				<button type="submit" name="subscribe" id="mc-embedded-subscribe" class="btn btn-gray-400"><i class="fas fa-caret-right"></i></button>
+				<button type="submit" name="subscribe" id="mc-embedded-subscribe" class="btn btn-gray-400"><i
+							class="fas fa-caret-right"></i></button>
 			</div>
 
 			<div id="mce-responses" class="clear">

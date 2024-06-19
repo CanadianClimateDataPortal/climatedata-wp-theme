411eef21d8168672b816ddfbf905b64e23122986
Sebastian Gonzalez <sebastian.gonzalez@evolvingweb.ca>
2024-06-19 17:41:44 -0500

    refs #65525: changes in logo and header socials.

--- fw-child/template/header-logo.php
+++ fw-child/template/header-logo.php
@@ -1,4 +1,4 @@
-<div id="header-logo-container" class="p-3 w-100">
+<div id="header-logo-container" class="p-2 p-xl-3 w-100">
 	<a href="<?php echo home_url(); ?>"><?php
 	
 		echo file_get_contents ( locate_template ( 'resources/img/climate-data-ca.svg' ) );

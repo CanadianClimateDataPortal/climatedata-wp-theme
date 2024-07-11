2a16953f1f04c731748710647f97cd94b1198538
Mahieddine Abdelkader <mahieddine.abdelkader@evolvingweb.com>
2024-07-10 22:36:06 -0400

    refs #65531: Fix meta sorting

--- fw-child/template/variable/query.php
+++ fw-child/template/variable/query.php
@@ -26,8 +26,8 @@
 			<div class="pt-6">
 				
 				<ul id="sort-menu" class="fw-query-sort list-unstyled d-flex all-caps">
-					<li class="selected me-4" data-sort="date_desc"><i class="fas fa-arrow-up me-2"></i> <?php _e ( 'Newest First', 'cdc' ); ?></li>
-					<li class="" data-sort="date_asc"><i class="fas fa-arrow-down me-2"></i> <?php _e ( 'Oldest First', 'cdc' ); ?></li>
+					<li class="selected me-4" data-sort="title_asc"><i class="fas fa-arrow-up me-2"></i> <?php _e ( 'Title (A–Z)', 'cdc' ); ?></li>
+					<li data-sort="title_desc"><i class="fas fa-arrow-down me-2"></i> <?php _e ( 'Title (Z–A)', 'cdc' ); ?></li>
 				</ul>
 				
 			</div>

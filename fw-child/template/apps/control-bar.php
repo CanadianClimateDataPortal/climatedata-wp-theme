23cbe8f6a860f61a58db772b8809fa887c434af2
domcode-ew <dominique.fournier@evolvingweb.com>
2024-07-23 08:04:08 -0300

    refs #67100: Make inner part of control-bar available for template.

--- fw-child/template/apps/control-bar.php
+++ fw-child/template/apps/control-bar.php
@@ -1,20 +1,5 @@
 <div class="col-2 col-sm-1">
 
-	<div id="control-bar" class="control-bar tab-drawer-tabs-container">
-
-		<div id="control-bar-tabs-footer" class="col-2 col-sm-1 text-center">
-		
-			<?php
-			$feedback_slug = 'feedback';
-
-			if ( $GLOBALS['fw']['current_lang_code'] == 'fr' )
-				$feedback_slug = 'retroaction';
-			?>
-
-			<a href="<?php echo home_url ( $feedback_slug ); ?>#apps-support" class="control-bar-text-link"><?php _e ( 'Feedback', 'cdc' ); ?></a>
-
-		</div>
-
-	</div>
+	<?php include ( 'control-bar-inner.php' ); ?>
 
 </div>

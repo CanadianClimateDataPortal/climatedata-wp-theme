8c22e075d79843e24f6c4d4e087cd913aa23dd83
Sebastian Gonzalez <sebastian.gonzalez@evolvingweb.ca>
2024-07-25 17:15:57 -0500

    refs #63108: adding styles for fake about us. Adding new social networks block.

---
+++ fw-child/resources/functions/builder/field-groups/block/data/social_networks.php
@@ -0,0 +1,38 @@
+<div class="accordion-item">
+	<h2 class="accordion-header" id="element-form-head-content">
+		<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#element-form-content" aria-expanded="true" aria-controls="element-form-content">
+			Social Networks
+		</button>
+	</h2>
+
+	<div id="element-form-content" class="accordion-collapse collapse show" aria-labelledby="element-form-head-content" data-bs-parent="#element-form">
+		<div class="accordion-body p-3">
+
+			<div class="row">
+				<div class="col">
+					<div>
+                        <span>LinkedIn</span>
+                        <input type="text" class="form-control" name="inputs-linkedin-url">
+					</div>
+					<div>
+                        <span>X (Twitter)</span>
+                        <input type="text" class="form-control" name="inputs-twitter-url">
+					</div>
+					<div>
+                        <span>Facebook</span>
+                        <input type="text" class="form-control" name="inputs-facebook-url">
+					</div>
+					<div>
+                        <span>Instagram</span>
+                        <input type="text" class="form-control" name="inputs-instagram-url">
+					</div>
+					<div>
+                        <span>SoundCloud</span>
+                        <input type="text" class="form-control" name="inputs-soundcloud-url">
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>

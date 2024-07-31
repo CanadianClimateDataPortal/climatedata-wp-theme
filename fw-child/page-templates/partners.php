18d67a0f5964e1a698e886d23f7629f734bcc3a3
Sebastian Gonzalez <sebastian.gonzalez@evolvingweb.ca>
2024-07-31 10:26:23 -0500

    refs #63108: partners template.

---
+++ fw-child/page-templates/partners.php
@@ -0,0 +1,33 @@
+<?php
+/**
+* Template Name: Partner Single Page
+*/
+
+	// update_option ( 'fwtest', array() );
+
+	get_header();
+
+	if ( have_posts() ) {
+		while ( have_posts() ) {
+			the_post();
+
+			// front-end output
+
+			fw_builder();
+
+			if ( current_user_can ( 'administrator' ) ) {
+
+?>
+
+<div id="output-btn" class="btn btn-danger">Show/Update Object</div>
+<pre id="output" class="bg-light m-3 p-3" style="font-size: 0.625rem;"></pre>
+
+<?php
+
+			}
+
+
+		}
+	}
+
+	get_footer();

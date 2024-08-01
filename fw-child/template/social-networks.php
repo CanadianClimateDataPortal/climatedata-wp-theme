23e05008b8139497c8c3b487a1a7b6988381086c
domcode-ew <dominique.fournier@evolvingweb.com>
2024-08-01 13:45:38 -0300

    refs #63108: Replace parent theme mods by built-in custom fields.

---
+++ fw-child/template/social-networks.php
@@ -0,0 +1,48 @@
+<div class="social-networks">
+    <ul>
+        <?php
+
+            // Associative array of social networks field name suffix and Font Awesome icon class
+            $social_networks = [
+                'linkedin' => 'fa-linkedin',
+                'twitter' => 'fa-x-twitter',
+                'facebook' => 'fa-facebook',
+                'instagram' => 'fa-instagram',
+                'soundcloud' => 'fa-soundcloud'
+            ];
+
+            foreach ( $social_networks as $network => $icon ) {
+                $field_value = get_field( 'social_link_' . $network );
+
+                if ( !empty( $field_value ) ) {
+
+        ?>
+
+                    <li><a href="<?php echo $field_value; ?>" target="_blank" rel="noopener"><i class="fa-brands <?php echo $icon; ?>"></i></a></li>
+
+        <?php
+
+                }
+            }
+
+
+            if ( is_user_logged_in() ) {
+
+                foreach ( $social_networks as $network => $icon ) {
+                    $field_value = get_field( 'social_link_' . $network );
+
+                    if ( empty( $field_value ) ) {
+
+                        // show warning if logged in
+
+        ?>
+
+            <div class="alert alert-warning fw-builder-alert">Custom field <code>social_link_<?php echo $network; ?></code> is empty. Click <span class="py-1 px-2 mx-1 text-bg-light"><i class="fas fa-pencil-alt me-2"></i>Edit in Wordpress</span> to add a value.</div>
+
+        <?php
+                    }
+                }
+            }
+        ?>
+    </ul>
+</div>

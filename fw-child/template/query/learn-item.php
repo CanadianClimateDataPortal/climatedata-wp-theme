8fee672a5bd05c97cd860738dc1d2f58564eadf9
Mahieddine Abdelkader <116840223+MahieddineAk@users.noreply.github.com>
2024-08-16 08:46:49 -0400

    #63094: Learning Zone improvments (#238)
    * refs #63094: Formatting
    
    * refs #63094: Clean variables
    
    * refs #63094: Extend CPTs on taxonomies used on filter
    
    * refs #63094: Update taxonomies configs for better UX
    
    * refs #63094: Update LZ main query
    
    * refs #63094: Remove sorting controls
    
    * refs #63094: Add script to init LZ ACF meta
    
    * refs #63094: Fix JS bug on meta filter
    
    * refs #63094: Remove sorting from learn query
    
    * refs #63094: Improve learn filter reset btn UI
    
    * refs #63094: Hide empty result
    
    * refs #63094: Allow scroll filter panel
    
    * refs #63094: Add global no matches message
    
    * refs #63094: Sort LZ items by date desc
    
    * refs #63094: Get only parent items.
    
    * refs #63094: Change filter hover color
    
    * refs #63094: Change LZ section title style
    
    * refs #63094: Add "Application" CT + Formatting
    
    * refs #63094: Improve LZ item style and logic
    
    * refs #63094: Update "Init LZ meta" script
    
    * refs #63094: Update news clear filter
    
    * refs #63094: Update translation
    
    * refs #63094: Translations, typos, css and admin menu.
    
    * refs #63094: Add translations
    
    * refs #63094: Adding translation fields + po/mo. Tiny visual fix.
    
    * refs #63094: Add taxo for var types
    
    * refs #63094: Adjust font of clear btn & hover color.
    
    * refs #63094: Update translations.
    
    * refs #63094: Fix default excerpt and comply to WCS.
    
    ---------
    
    Co-authored-by: domcode-ew <dominique.fournier@evolvingweb.com>

--- fw-child/template/query/learn-item.php
+++ fw-child/template/query/learn-item.php
@@ -6,92 +6,104 @@ if ( isset( $item['lang'] ) && in_array( $item['lang'], array( 'en', 'fr' ), tru
 	$current_lang = $item['lang'];
 }
 
-$native_excerpt = apply_filters ( 'the_content', custom_excerpt ( 20, $item['id'] ) );
+$native_excerpt = custom_excerpt( 20, $item['id'] );
 
 $excerpts = array (
-    'en' => $native_excerpt ? get_field( 'excerpt', $item['id'] ) : get_field( 'excerpt', $item['id'] ), // Overrides native excerpt with custom excerpt if present
-    'fr' => get_field( 'excerpt_fr', $item['id'] ) ? get_field( 'excerpt_fr', $item['id'] ) : $native_excerpt, // Defaults back to native excerpt if no custom FR not present
+	'en' => get_field( 'excerpt', $item['id'] ) ? get_field( 'excerpt', $item['id'] ) : $native_excerpt, // Overrides native excerpt with custom excerpt if present
+	'fr' => get_field( 'excerpt_fr', $item['id'] ) ? get_field( 'excerpt_fr', $item['id'] ) : $native_excerpt, // Defaults back to native excerpt if no custom FR not present
 );
 ?>
 
-<div class="card">
-	<?php
-	
-		if ( has_post_thumbnail ( $item['id'] ) ) {
-			
-	?>
-	
-	<div class="bg-dark">
-		<div class="card-img item-thumb" style="background-image: url(<?php echo get_the_post_thumbnail_url( $item['id'], 'medium_large' ); ?>);"></div>
-	</div>
-	
-	<?php
-	
-		}
+<div class="card card--learn">
 
+	<?php
+	if ( has_post_thumbnail( $item['id'] ) ) { ?>
+		<div class="bg-dark">
+			<div class="card-img item-thumb" style="background-image: url(<?php echo get_the_post_thumbnail_url( $item['id'], 'medium_large' ); ?>);"></div>
+		</div>
+		<?php
+	}
 	?>
-	
-	<div class="card-body">
-		<h5 class="card-title item-title"><a href="<?php echo $item['permalink']; ?>" class="text-secondary stretched-link"><?php echo $item['title']; ?></a></h5>
-		
-		<div class="mb-3">
+
+	<div class="card-body d-flex flex-column">
+		<h5 class="card-title item-title">
+			<a href="<?php echo $item['permalink']; ?>" class="text-secondary stretched-link">
+				<?php echo $item['title']; ?>
+			</a>
+		</h5>
+
+		<div class="card-desc text-gray-600">
 			<?php
-			
-				echo wp_kses_post( $excerpts[ $item['lang'] ] );
-			
+
+				echo wp_kses_post( $excerpts[ $current_lang ] );
+
 			?>
 		</div>
-		
-		<div class="row row-cols-2">
+
+		<?php
+		// Initialize card format and time.
+		$card_asset_type = get_field( 'asset_type', $item['id'] );
+		$card_asset_time = get_field( 'asset_time', $item['id'] );
+		?>
+
+		<div class="row row-cols-2 pt-3 mt-auto">
 			<div class="col">
-				<h6 class="all-caps text-secondary"><?php _e ( 'Format', 'cdc' ); ?></h6>
-	
+				<h6 class="all-caps fw-bold"><?php _e( 'Format', 'cdc' ); ?></h6>
+
 				<?php
-				
-					$format_icon = '';
-					$format_name = get_field ( 'asset_type', $item['id'] );
-					
-					switch ( get_field ( 'asset_type', $item['id'] ) ) {
-						
-						case 'video' :
-							$format_icon = 'fas fa-video';
-							$format_name = __ ( 'Video', 'cdc' );
-							break;
-							
-						case 'audio' :
-							$format_icon = 'fas fa-volume';
-							$format_name = __ ( 'Audio', 'cdc' );
-							break;
-							
-						case 'interactive' :
-							$format_icon = 'far fa-hand-pointer';
-							$format_name = __ ( 'Interactive', 'cdc' );
-							break;
-							
-						case 'article' :
-							$format_icon = 'far fa-newspaper';
-							$format_name = __ ( 'Article', 'cdc' );
-							break;
-							
-					}
-					
+				$format_icon = '';
+				$format_name = '';
+
+				switch ( $card_asset_type ) {
+					case 'video' :
+						$format_icon = 'fas fa-video';
+						$format_name = __( 'Video', 'cdc' );
+
+						break;
+					case 'audio' :
+						$format_icon = 'fas fa-microphone';
+						$format_name = __( 'Audio', 'cdc' );
+
+						break;
+					case 'interactive' :
+						$format_icon = 'far fa-hand-pointer';
+						$format_name = __( 'Interactive', 'cdc' );
+
+						break;
+					case 'app' :
+						$format_icon = 'far fa-window-maximize';
+						$format_name = __( 'App', 'cdc' );
+
+						break;
+					default : // Article.
+						$format_icon = 'far fa-newspaper';
+						$format_name = __( 'Article', 'cdc' );
+
+						break;
+				}
 				?>
-				
-				<p class="mb-0 small text-gray-600">
-					<i class="<?php echo $format_icon; ?> me-2"></i><?php echo $format_name; ?>
+
+				<p class="card-asset-type mb-0 text-gray-600 d-flex align-items-center">
+					<i class="<?php echo $format_icon; ?> me-2"></i>
+					<span><?php echo $format_name; ?></span>
 				</p>
 			</div>
-			
-			<div class="col">
-				<h6 class="all-caps text-secondary"><?php _e ( 'Time', 'cdc' ); ?></h6>
-			
-				<p class="mb-0 small text-gray-600"><?php
-				
-					echo get_field ( 'asset_time', $item['id'] ) . ' minutes';
-				
-				?></p>
-			</div>
+
+			<?php
+			if ( ! empty( $card_asset_time ) ) {
+				?>
+				<div class="col">
+					<h6 class="all-caps fw-bold"><?php _e( 'Time', 'cdc' ); ?></h6>
+
+					<p class="card-asset-time mb-0 text-gray-600 d-flex align-items-center">
+						<?php
+						echo sprintf( __( '%s minutes', 'cdc' ), $card_asset_time);
+						?>
+					</p>
+				</div>
+				<?php
+			}
+			?>
 		</div>
-		
 	</div>
 </div>

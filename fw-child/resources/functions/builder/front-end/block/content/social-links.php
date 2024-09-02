71edbbc722a106633fa23521c37123e4e436cc09
xfrenette-crim <159493515+xfrenette-crim@users.noreply.github.com>
2024-09-01 20:49:01 -0400

    CLIM-732: "Social links" block: add option to use brand colors (#278)
    Add a checkbox in the "Social links" Builder block to be able to display the social links using their "brand" color instead of the default link color.

--- fw-child/resources/functions/builder/front-end/block/content/social-links.php
+++ fw-child/resources/functions/builder/front-end/block/content/social-links.php
@@ -13,30 +13,37 @@ $networks = [
 	'linkedin' => [
 		'icon' => 'fa-linkedin',
 		'name' => 'LinkedIn',
+		'brand-color' => '#0a66c2',
 	],
 	'x-twitter' => [
 		'icon' => 'fa-x-twitter',
 		'name' => 'X',
+		'brand-color' => '#000000',
 	],
 	'facebook' => [
 		'icon' => 'fa-facebook',
 		'name' => 'Facebook',
+		'brand-color' => '#1877f2',
 	],
 	'instagram' => [
 		'icon' => 'fa-instagram',
 		'name' => 'Instagram',
+		'brand-color' => '#c13584',
 	],
 	'soundcloud' => [
 		'icon' => 'fa-soundcloud',
 		'name' => 'SoundCloud',
+		'brand-color' => '#ff5500',
 	],
 ];
 
 $links = [];
 $show_network_names = $element[ 'inputs' ][ 'show_name' ] ?? false;
-// The builder may save the value as the string "true" or the boolean `true`,
+$use_brand_colors = $element[ 'inputs' ][ 'brand_colored' ] ?? false;
+// The builder may save checkbox values as the string "true" or the boolean `true`,
 // so we normalize.
 $show_network_names = ( true === $show_network_names || "true" === $show_network_names );
+$use_brand_colors = ( true === $use_brand_colors || "true" === $use_brand_colors );
 
 if ( isset( $element[ 'inputs' ][ 'links' ][ 'rows' ] ) ) {
 
@@ -51,17 +58,23 @@ if ( isset( $element[ 'inputs' ][ 'links' ][ 'rows' ] ) ) {
 
 		$network = $networks[ $row[ 'network' ] ];
 
-		$links[] = [
+		$link_data = [
 			'url' => $row[ 'url' ][ $lang ],
 			'icon' => $network[ 'icon' ],
 			'label' => $network[ 'name' ],
 		];
+
+		if ( $use_brand_colors ) {
+			$link_data[ 'brand-color' ] = $network[ 'brand-color' ];
+		}
+
+		$links[] = $link_data;
 	}
 }
 
 ?>
 
-<div class="social-networks">
+<div class="social-networks <?php echo $use_brand_colors ? 'brand-colored' : '' ?>">
 	<?php if ( ! empty( $links ) ) : ?>
 		<ul>
 			<?php foreach ( $links as $link ) : ?>
@@ -71,6 +84,9 @@ if ( isset( $element[ 'inputs' ][ 'links' ][ 'rows' ] ) ) {
 						target="_blank"
 						rel="noopener"
 						title="<?php echo esc_attr( $link[ 'label' ] ) ?>"
+						<?php if ( ! empty( $link[ 'brand-color' ] ) ) : ?>
+							style="color: <?php echo $link[ 'brand-color' ] ?>;"
+						<?php endif; ?>
 					>
 						<i class="fa-brands <?php echo $link[ 'icon' ] ?>"></i>
 						<?php if ( $show_network_names ) : ?>

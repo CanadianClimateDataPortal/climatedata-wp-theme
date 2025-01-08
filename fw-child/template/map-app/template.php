8f59e6f6dc415db3dce759b6252f46cbe2c2bfc8
Mahieddine Abdelkader <mahieddine.abdelkader@evolvingweb.com>
2025-01-07 23:54:43 -0500

    refs #76250: Add lang directive

--- fw-child/template/map-app/template.php
+++ fw-child/template/map-app/template.php
@@ -3,5 +3,16 @@
  * Map app template.
  */
 
+// Initialize current language.
+$current_lang = 'en';
+
+if (
+	isset( $GLOBALS['fw'] )
+	&& isset( $GLOBALS['fw']['current_lang_code'] )
+	&& in_array( $GLOBALS['fw']['current_lang_code'], array( 'en', 'fr' ), true )
+) {
+	$current_lang = $GLOBALS['fw']['current_lang_code'];
+}
+
 // Render the root container for the map app.
-echo '<div id="root"></div>';
+echo '<div id="root" data-app-lang="' . $current_lang . '"></div>';

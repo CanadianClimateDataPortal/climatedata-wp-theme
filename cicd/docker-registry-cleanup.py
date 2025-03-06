667270cbc37ce7affba96eda71cdb98623bf03f5
Xavier Frenette <xavier.frenette@crim.ca>
2025-03-06 17:20:17 -0500

    Added pagination for API Various comments improvements Various naming improvements

--- cicd/docker-registry-cleanup.py
+++ cicd/docker-registry-cleanup.py
@@ -7,17 +7,10 @@ reserved_tags = ["preprod", "uat", "qa"]
 
 class RepositoryAPI:
     """
-    A class to interact with the GitLab API of a specific container registry and manage Docker image tags in a container registry.
+    A class to interact with the GitLab API of a specific container registry and manage Docker image its tags.
 
     This class allows users to retrieve information about repository tags, delete old tags, and perform
     various operations related to tags in a GitLab Container Registry.
-
-    Attributes:
-    - api_url (str): The base URL of the GitLab API.
-    - project_id (str): The ID of the GitLab project.
-    - repository_name (str): The name of the repository to interact with.
-    - _headers (dict): HTTP headers used for API requests (contains the authentication token).
-    - _repo_endpoint (str): The endpoint for accessing the container registry repository.
     """
 
     def __init__(self, token, api_url, project_id, repository_name):
@@ -25,15 +18,15 @@ class RepositoryAPI:
         Initializes the RepositoryAPI instance with authentication headers and repository details.
 
         Args:
-        - token (str): GitLab API authentication token.
-        - api_url (str): The base URL of the GitLab API.
-        - project_id (str): The ID of the GitLab project.
-        - repository_name (str): The name of the repository.
+          token (str): GitLab API authentication token.
+          api_url (str): The base URL of the GitLab API.
+          project_id (str): The ID of the GitLab project.
+          repository_name (str): The name of the repository.
         """
         self._headers = {"PRIVATE-TOKEN": token}
-        self.repository_name = repository_name
-        self.api_url = api_url
-        self.project_id = project_id
+        self._repository_name = repository_name
+        self._api_url = api_url
+        self._project_id = project_id
         self._repo_endpoint = self._get_repository_endpoint()
 
     def _make_api_request(self, method, endpoint):
@@ -41,74 +34,85 @@ class RepositoryAPI:
         Makes an HTTP request to the GitLab API using the specified method and endpoint.
 
         Args:
-        - method (str): The HTTP method to use.
-        - endpoint (str): The API endpoint relative to the base URL.
+          method (str): The HTTP method to use.
+          endpoint (str): The API endpoint relative to the base URL.
 
         Returns:
-        - dict: The parsed response data as a Python dictionary, containing the data returned by the API.
+          The response.
 
         Raises:
-        - requests.HTTPError: If the request fails.
+          requests.HTTPError: If the request fails.
         """
         response = requests.request(
-            method, f"{self.api_url}/{endpoint}", headers=self._headers
+            method, f"{self._api_url}/{endpoint}", headers=self._headers
         )
         if response.status_code != 200:
             raise requests.HTTPError(
-                f"Error {response.status_code} retrieving data from {self.api_url}{endpoint}: {response.text}"
+                f"Error {response.status_code} retrieving data from {self._api_url}{endpoint}: {response.text}"
             )
-        return response.json()
+        return response
 
     def _get_repository_endpoint(self):
         """
         Retrieves the endpoint for a specific registry repository within a GitLab project.
 
         Returns:
-        - str: The endpoint of the matching repository if found.
+          str: The endpoint of the matching repository if found.
 
         Raises:
-        - ValueError: If the repository with the specified name is not found in the project.
+          ValueError: If the repository with the specified name is not found in the project.
         """
-        repo_endpoint = f"{self.project_id}/registry/repositories"
-        repositories = self._make_api_request("GET", repo_endpoint) or []
+        repo_endpoint = f"{self._project_id}/registry/repositories"
+        response = self._make_api_request("GET", repo_endpoint)
+        repositories = response.json()
 
-        repo = next(
-            (repo for repo in repositories if repo.get("name") == self.repository_name),
-            None,
-        )
-
-        if repo:
+        try:
+            repos_with_matching_name = (repo for repo in repositories if repo.get("name") == self._repository_name)
+            repo = next(repos_with_matching_name)
             return f"{repo_endpoint}/{repo['id']}"
-        else:
-            raise ValueError(f"No repository named '{self.repository_name}' found.")
+        except StopIteration:
+            raise ValueError(f"No repository named '{self._repository_name}' found.")
+
 
     def _get_tag_info(self, tag):
         """
         Retrieves detailed information (digest and creation date) for a specific tag in the repository.
 
         Args:
-        - tag (str): The name of the tag to query.
+          tag (str): The name of the tag to query.
 
         Returns:
-        - tuple: A tuple containing the digest and the creation date of the tag.
+          tuple: A tuple containing the digest and the creation date of the tag.
         """
         endpoint = f"{self._repo_endpoint}/tags/{tag}"
 
-        tag_info = self._make_api_request("GET", endpoint)
+        response = self._make_api_request("GET", endpoint)
+        tag_info = response.json()
         return tag_info.get("digest"), tag_info.get("created_at")
 
-    def _get_all_tags_with_infos(self):
+    def get_all_tags_with_infos(self):
         """
         Retrieves all tags available in the repository.
 
         Returns:
-        - list: A list of dictionaries of all tag with their metadata.
+          list[dict]: A list of dictionaries of all tag with their metadata.
 
         Raises:
-        - ValueError: If no tags are found.
+          ValueError: If no tags are found.
         """
+        tags = []
         tags_infos = []
-        tags = self._make_api_request("GET", f"{self._repo_endpoint}/tags") or []
+
+        next_page = 1
+        has_next_page = True
+
+        while has_next_page:
+            response = self._make_api_request("GET", f"{self._repo_endpoint}/tags?page={next_page}")
+            tags.extend(response.json())
+            total_nb_pages = int(response.headers.get("x-total-pages"))
+            has_next_page = next_page < total_nb_pages
+            next_page += 1
+
         if not tags:
             raise ValueError("No tags found for this repository.")
 
@@ -121,35 +125,31 @@ class RepositoryAPI:
 
         return tags_infos
 
-    def _delete_tags(self, tags):
+    def delete_tags(self, tags):
         """
         Deletes the specified tags from the GitLab repository.
 
         Args:
-        - tags (list): The list of tags to delete.
+          tags (list): The list of tags to delete.
         """
         for tag in tags:
-            print(
-                f"Tag: {tag['name']}, Digest: {tag['digest']}, Created At: {tag['created_at']}"
-            )
-            # tag_name = tag['name']}
-            # delete_endpoint = f"{self._get_repository_endpoint()}/tags/{tag_name}"
-            # response = self._make_api_request("DELETE", delete_endpoint)
-            # if response == {}:
-            #     print(f"Successfully deleted tag: {tag_name}")
-            # else:
-            #     print(f"Failed to delete tag: {tag_name}.")
+            tag_name = tag['name']
+            delete_endpoint = f"{self._repo_endpoint}/tags/{tag_name}"
+            self._make_api_request("DELETE", delete_endpoint)
 
 
-def filter_tags(tags):
+def filter_required_tags(tags):
     """
-    Filters out reserved tags and returns only non-important tags.
+    Filters out tags that must be kept.
+
+    Tags that must be kept are the "reserved" tags and all the other tags of the same
+    Docker image.
 
     Args:
-    - tags (list): List of all tags with their digests and creation dates retrieved from the repository.
+      tags (list): List of all tags with their digests and creation dates retrieved from the repository.
 
     Returns:
-    - list: A list of non-important tags with their informations.
+      list: A list of tags to keep.
     """
     protected_digests = []
     filtered_tags = []
@@ -170,11 +170,11 @@ def get_old_tags(tags, n):
     Returns the tags older than the n most recent ones, sorted by creation date.
 
     Args:
-    - tags (list): A list of tags with their associated metadata (e.g., digest and creation date).
-    - n (int): The number of most recent tags to retain.
+      tags (list): A list of tags with their associated metadata (e.g., digest and creation date).
+      n (int): The number of most recent tags to retain.
 
     Returns:
-    - list: A list of tags older than the n most recent ones.
+      list: A list of tags older than the n most recent ones.
     """
     sorted_tags = sorted(
         tags,
@@ -199,21 +199,44 @@ def main():
         help="ID of the GitLab project hosting the Container Registry to clean",
     )
     parser.add_argument(
+        "--repository-name",
+        default="portal",
+        help="Name of the repository to clean",
+    )
+    parser.add_argument(
         "--nb-to-keep",
         type=int,
         required=True,
         help="Number of most recent tags to keep",
     )
+    parser.add_argument(
+        "--yes",
+        action="store_true",
+        help="Do not ask for confirmation before deleting tags",
+    )
 
     args = parser.parse_args()
-    repository_name = "portal"
-    repo_api = RepositoryAPI(args.token, args.api_url, args.project_id, repository_name)
 
     try:
-        tags = repo_api._get_all_tags_with_infos()
-        filtered_tags = filter_tags(tags)
+        repo_api = RepositoryAPI(
+            args.token, args.api_url, args.project_id, args.repository_name
+        )
+        print("Retrieving tags from the repository...")
+        tags = repo_api.get_all_tags_with_infos()
+        filtered_tags = filter_required_tags(tags)
         tags_to_delete = get_old_tags(filtered_tags, args.nb_to_keep)
-        repo_api._delete_tags(tags_to_delete)
+
+        if not tags_to_delete:
+            print("No tags to delete.")
+            return
+
+        print(f"The following tags will be deleted: {', '.join(t['name'] for t in tags_to_delete)}")
+        do_delete = args.yes or input("Continue? [y/N]: ").lower() in ["y", "yes"]
+        if do_delete:
+            repo_api.delete_tags(tags_to_delete)
+            print(f"Deleted {len(tags_to_delete)} tags")
+        else:
+            print("Aborted.")
     except requests.HTTPError as http_err:
         print(f"HTTP error occurred: {http_err}")
     except ValueError as val_err:

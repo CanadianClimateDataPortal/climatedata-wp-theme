08f355bb147d34c0ad051ad9fe51459919d01e73
Ali Demi <52844766+alinademi@users.noreply.github.com>
2025-04-07 07:47:41 -0700

    refs: #81974: output breadcrumb chain dynamically (#360)
    * refs: #81974: output breadcrumb chain dynamically
    
    based on the api response.
    adds a fallback if terms in response are empty. Needs double check.
    does not seem ideal.
    
    * Refs: #81974: Get the dataset name from the state instead of API
    
    * Refs: #81974: get the variable data from the state instead of the API
    
    remove unnecessary types, since we already have usable ones
    
    * Refs: #81974: get the variable title in breadcrumb instead of id
    
    * Refs: 81974: make the variable title multilingual
    
    added better client side locale detection to locale provider
    updated `ClimateVariableBase` for multilingual title
    enhanced `ClimateVariableConfigInterface` for multilingual title
    
    * Refs: 81974: remove -- from useMemo dependencies
    
    * Refs: 81974: update prettierrc to match editor config
    
    we are using esling prettier plugin in eslintrc, but just in case, added
    necessities to prettierrc to be picked up by editor extension
    
    * Refspec: 81974: remove the title prop and initial load dummy response
    
    from Breadcrumbs component.

--- apps/src/types/climate-variable-interface.ts
+++ apps/src/types/climate-variable-interface.ts
@@ -1,4 +1,5 @@
 import React from "react";
+import { MultilingualField } from "./types";
 
 export interface ScenariosConfig {
 	[key: string]: string[];
@@ -100,6 +101,9 @@ export interface ClimateVariableConfigInterface {
 
 	/** WordPress Post ID, used for backend operations (optional) */
 	postId?: number;
+
+	/** Title of the climate variable from the API */
+	title?: string | MultilingualField;
 
 	/** Class name defining the type or category of the climate variable */
 	class: string;
@@ -207,6 +211,8 @@ export interface ClimateVariableConfigInterface {
  */
 export interface ClimateVariableInterface {
 	getId(): string;
+
+	getTitle(): string | null;
 
 	getVersions(): string[];
 

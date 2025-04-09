ffc47f5f48840e60d08659074077b07c6df24cc0
don-ew <98974959+don-ew@users.noreply.github.com>
2025-04-09 13:31:55 -0400

    refs #81999: Map time period slider (#374)
    * refs #81999: Retrieve year from the climate variable slice and use is to generate the map.
    
    * refs #81999: Tweaked how the date range is displayed.
    
    * refs #81999: Tweaked the sidebar styles to ensure that the slider tooltip remains visible.
    
    * - refs #81999: Added support for other regions (health, census, watersheds).
    
    * - refs #81999: Added default date range values.
    
    * - refs #81999: Removed event handlers from dependencies causing the map to be re-rendered unnecessarily.

--- apps/src/components/map-layers/interactive-regions-layer.tsx
+++ apps/src/components/map-layers/interactive-regions-layer.tsx
@@ -29,7 +29,6 @@ const InteractiveRegionsLayer: React.FC = () => {
 	const map = useMap();
 
 	const {
-		decade,
 		legendData,
 	} = useAppSelector((state) => state.map);
 
@@ -206,12 +205,13 @@ const InteractiveRegionsLayer: React.FC = () => {
 			}
 
 			const frequency = climateVariable?.getFrequency() ?? '';
+			const [ startYear ] = climateVariable?.getDateRange() ?? [];
 
 			try {
 				const data = await fetchChoroValues({
 					variable: climateVariable?.getThreshold() ?? '',
 					dataset: climateVariable?.getVersion() ?? '',
-					decade,
+					decade: startYear,
 					frequency,
 					interactiveRegion,
 					emissionScenario: climateVariable?.getScenario() ?? '',
@@ -224,7 +224,6 @@ const InteractiveRegionsLayer: React.FC = () => {
 			}
 		})();
 	}, [
-		decade,
 		climateVariable,
 	]);
 
@@ -270,16 +269,18 @@ const InteractiveRegionsLayer: React.FC = () => {
 			map.removeLayer(layer);
 			layerRef.current = null;
 		};
-	}, [
-		map,
-		interactiveRegion,
-		layerData,
-		tileLayerUrl,
-		vectorTileLayerStyles,
-		handleClick,
-		handleOver,
-		handleOut,
-	]);
+	},
+		// Intentionally not including the event handlers (e.g. handleClick)
+		// as dependencies since they are already being handled by the
+		// userInteractiveMapEvents hook.
+		[
+			map,
+			interactiveRegion,
+			layerData,
+			tileLayerUrl,
+			vectorTileLayerStyles,
+		]
+	);
 
 	return null;
 };
--- apps/src/components/map-layers/variable-layer.ts
+++ apps/src/components/map-layers/variable-layer.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 import L from 'leaflet';
 import { useMap } from 'react-leaflet';
 import { useAppSelector } from '@/app/hooks';
@@ -22,12 +22,12 @@ interface VariableLayerProps {
 export default function VariableLayer({ layerValue }: VariableLayerProps): null {
 	const map = useMap();
 	const {
-		decade,
 		pane,
 		opacity: { mapData },
 	} = useAppSelector((state) => state.map);
 
 	const { climateVariable } = useClimateVariable();
+	const [ startYear ] = useMemo(() => climateVariable?.getDateRange() ?? [], [climateVariable]);
 
 	const layerRef = useRef<L.TileLayer.WMS | null>(null);
 
@@ -43,7 +43,7 @@ export default function VariableLayer({ layerValue }: VariableLayerProps): null 
 			version: climateVariable?.getVersion() === 'cmip6' ? '1.3.0' : '1.1.1',
 			layers: layerValue,
 			styles: climateVariable?.getLayerStyles(),
-			TIME: parseInt(decade) + '-01-00T00:00:00Z',
+			TIME: parseInt(startYear) + '-01-00T00:00:00Z',
 			opacity: 1,
 			pane: pane,
 			bounds: CANADA_BOUNDS,
@@ -58,7 +58,7 @@ export default function VariableLayer({ layerValue }: VariableLayerProps): null 
 			newLayer.addTo(map);
 			layerRef.current = newLayer;
 		}
-	}, [layerValue, map, climateVariable, decade, pane]);
+	}, [layerValue, map, climateVariable, pane, startYear]);
 
 	useEffect(() => {
 		if (layerRef.current) {

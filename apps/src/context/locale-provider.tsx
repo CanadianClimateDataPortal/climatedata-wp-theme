9a172c054881c54ee1b0acb234dc74e73da5f6d9
Pierre-Paul Lefebvre <pierre-paul.lefebvre@evolvingweb.com>
2025-05-01 13:04:57 -0400

    refs #85760: remove browser language.

--- apps/src/context/locale-provider.tsx
+++ apps/src/context/locale-provider.tsx
@@ -21,14 +21,6 @@ export const LocaleProvider: React.FC<{ children: React.ReactNode }> = ({
 
 	const [locale, setLocale] = useState<Locale>(initialLocale);
 
-	useEffect(() => {
-		// Client-side locale
-		const browserLocale = navigator.language;
-		if (browserLocale.startsWith('fr')) {
-			setLocale('fr');
-		}
-	}, []);
-
 	return (
 		<LocaleContext.Provider value={{ locale, setLocale }}>
 			{children}

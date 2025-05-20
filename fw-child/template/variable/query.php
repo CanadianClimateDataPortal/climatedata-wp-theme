875cb92f2c22cb8549f7dc4ee853077816f1de4f
Xavier Frenette <xavier.frenette@crim.ca>
2025-05-20 17:48:26 -0400

    CLIM-976: Add option to control visibility in the "Variables" page
    Add an ACF checkbox field in the Variable edit screen to control if a variable is to be shown in the "Variables" listing page.

--- fw-child/template/variable/query.php
+++ fw-child/template/variable/query.php
@@ -15,17 +15,17 @@ $is_fr = 'fr' === $current_lang;
 
 <div class="col row bg-gray-200 position-relative z-0">
 	<?php
-	
+
 		//
 		// CONTROL BAR
 		//
-		
+
 		include ( locate_template ( 'template/variable/control-bar.php' ) );
-		
+
 		//
 		// QUERY
 		//
-		
+
 		$item_options = array (
 			'type' => 'items',
 			'template' => 'template/query/variable-item.php',
@@ -33,9 +33,9 @@ $is_fr = 'fr' === $current_lang;
 			'class' => array ( 'row', 'row-cols-3' ),
 			'item_class' => 'col'
 		);
-		
+
 	?>
-	
+
 	<div class="col-12 offset-1 col-sm-13">
 
 		<?php
@@ -48,7 +48,13 @@ $is_fr = 'fr' === $current_lang;
 				'post_parent'    => 0,
 				'post_status'    => 'publish',
 				'orderby'        => 'title',
-				'order'          => 'asc'
+				'order'          => 'asc',
+				'meta_query'     => array(
+					array(
+						'key'   => 'show_in_listing',
+						'value' => '1',
+					),
+				),
 			);
 
 			if ( $is_fr ) {
@@ -61,24 +67,24 @@ $is_fr = 'fr' === $current_lang;
 		?>
 
 		<div class="pt-6">
-			
+
 			<ul id="sort-menu" class="fw-query-sort list-unstyled d-flex all-caps">
 				<li class="selected me-4" data-sort="<?php echo $sort_asc; ?>"><i class="fas fa-arrow-up me-2"></i> <?php _e ( 'Name (A–Z)', 'cdc' ); ?></li>
 				<li data-sort="<?php echo $sort_desc; ?>"><i class="fas fa-arrow-down me-2"></i> <?php _e ( 'Name (Z–A)', 'cdc' ); ?></li>
 			</ul>
-			
+
 		</div>
 
 		<div id="variable-grid" class="py-6" data-args='<?php echo json_encode ( $variables_args ); ?>'>
-			
+
 			<div class="query-container">
 				<div class="fw-query-items row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 g-lg-6" data-options='<?php echo json_encode ( $item_options ); ?>'>
-					
+
 					<div class="fw-query-item"></div>
-				
+
 				</div>
 			</div>
 		</div>
 	</div>
-	
+
 </div>

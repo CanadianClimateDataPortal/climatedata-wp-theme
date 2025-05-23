1f3a70e6d7fc511d66138e5336c03b92d0980459
Xavier Frenette <xavier.frenette@crim.ca>
2025-05-23 13:11:52 -0400

    CLIM-925: Linting fix

--- apps/src/lib/allowance-climate-variable.tsx
+++ apps/src/lib/allowance-climate-variable.tsx
@@ -1,5 +1,5 @@
 import MarineClimateVariable from "@/lib/marine-climate-variable";
-import MedianOnlyVariableValues from "@/components/map-layers/median-only-variable-values.tsx";
+import MedianOnlyVariableValues from "@/components/map-layers/median-only-variable-values";
 
 class AllowanceClimateVariable extends MarineClimateVariable {
 	getLocationModalContent(latlng: L.LatLng, featureId: number, mode: "modal" | "panel" = "modal"): React.ReactNode {

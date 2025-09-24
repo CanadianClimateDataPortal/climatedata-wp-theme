1b16e3fe8b8cd9663e64b39e5a1ac6840bfb5a82
Renoir Boulanger <contribs@renoirboulanger.com>
2025-09-24 14:36:16 -0400

    CLIM-1145: Fix: Missing a dash between "30" and "year" (#616)
    * fix(i18n): map 30-year range
    * fix(i18n): adjust this too?
    * fix: consistency in comments
    
    ---------
    
    Co-authored-by: Renoir Boulanger <renoir.boulanger@crim.ca>

--- apps/src/components/download/step-additional-details.tsx
+++ apps/src/components/download/step-additional-details.tsx
@@ -164,7 +164,7 @@ const StepAdditionalDetails = React.forwardRef<StepComponentRef>((_, ref) => {
 		},
 		{
 			value: AveragingType.THIRTY_YEARS,
-			label: __('30 year averages'),
+			label: __('30-year averages'),
 		},
 	].filter((option) =>
 		climateVariable?.getAveragingOptions()?.includes(option.value)

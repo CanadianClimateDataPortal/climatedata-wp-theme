7f9d86f6bc9babadbd35dcbeea9e7fee6090e0f2
Renoir Boulanger <renoir.boulanger@crim.ca>
2025-10-01 17:43:40 -0400

    CLIM-1168: redux store state

--- apps/src/store/seasonal-decadal-slice.ts
+++ apps/src/store/seasonal-decadal-slice.ts
@@ -1,8 +1,12 @@
-import { createSlice } from '@reduxjs/toolkit';
+import {
+	createSlice,
+	type Reducer,
+ } from '@reduxjs/toolkit';
+import { __ } from '@/context/locale-provider';
 
 const VALUES_VAR = [
-	'air_temp',
-	'precipitation',
+	['air_temp', __('Temperature')],
+	['precipitation', __('Precipitation')],
 ] as const;
 
 /**
@@ -12,23 +16,23 @@ const VALUES_VAR = [
  * - air_temp
  * - precipitation
  */
-export const S2D_VAR = [...VALUES_VAR] as const;
+export const S2D_VAR: ReadonlyMap<S2DParamVar, string> = new Map([...VALUES_VAR]);
 
 /**
  * Type representing the valid variable identifiers for seasonal/decadal data.
  *
  * @see S2D_VAR for the source array of valid values.
  */
-export type S2DParamVar = (typeof VALUES_VAR)[number];
+export type S2DParamVar = (typeof VALUES_VAR)[number][0];
 
-const DEFAULT_VALUE_VAR = VALUES_VAR[0];
+const DEFAULT_VALUE_VAR = VALUES_VAR[0][0];
 
 // -----
 
 const VALUES_FREQ = [
-	'monthly',
-	'seasonal',
-	'decadal',
+	['monthly', __('Monthly') ], 
+	['seasonal', __('Seasonal (3 months)') ], 
+	['decadal', __('Decadal (5 years)') ], 
 ] as const;
 
 /**
@@ -39,22 +43,22 @@ const VALUES_FREQ = [
  * - seasonal
  * - decadal
  */
-export const S2D_FREQ = [...VALUES_FREQ] as const;
+export const S2D_FREQ: ReadonlyMap<S2DParamFreq, string> = new Map(VALUES_FREQ);
 
 /**
  * Type representing the valid variable identifiers for seasonal/decadal data.
  *
  * @see S2D_FREQ for the source array of valid values.
  */
-export type S2DParamFreq = (typeof VALUES_FREQ)[number];
+export type S2DParamFreq = (typeof VALUES_FREQ)[number][0];
 
-const DEFAULT_VALUE_FREQ = VALUES_FREQ[0];
+const DEFAULT_VALUE_FREQ = VALUES_FREQ[0][0];
 
 // -----
 
 const VALUES_FORECAST_TYPE = [
-	'expected_conditions',
-	'unusual_conditions',
+	['expected_conditions', __('Expected Conditions')],
+	['unusual_conditions', __('Unusual Conditions')],
 ] as const;
 
 /**
@@ -62,22 +66,22 @@ const VALUES_FORECAST_TYPE = [
  * - expected_conditions
  * - unusual_conditions
  */
-export const S2D_FORECAST_TYPE = [...VALUES_FORECAST_TYPE] as const;
+export const S2D_FORECAST_TYPE: ReadonlyMap<S2DParamForecastType, string> = new Map([...VALUES_FORECAST_TYPE]);
 
 /**
  * Type representing the valid forecast type identifiers for seasonal/decadal data.
  *
  * @see S2D_FORECAST_TYPE for the source array of valid values.
  */
-export type S2DParamForecastType = (typeof VALUES_FORECAST_TYPE)[number];
+export type S2DParamForecastType = (typeof VALUES_FORECAST_TYPE)[number][0];
 
-const DEFAULT_VALUE_FORECAST_TYPE = VALUES_FORECAST_TYPE[0];
+const DEFAULT_VALUE_FORECAST_TYPE = VALUES_FORECAST_TYPE[0][0];
 
 // -----
 
 const VALUES_FORECAST_DISPLAY = [
-	'forecast',
-	'climatology',
+	['forecast', __('Forecast')],
+	['climatology', __('Climatology')],
 ] as const;
 
 /**
@@ -85,11 +89,11 @@ const VALUES_FORECAST_DISPLAY = [
  * - forecast
  * - climatology
  */
-export const S2D_FORECAST_DISPLAY = [...VALUES_FORECAST_DISPLAY] as const;
+export const S2D_FORECAST_DISPLAY: ReadonlyMap<S2DParamForecastDisplay, string> = new Map([...VALUES_FORECAST_DISPLAY]);
 
-export type S2DParamForecastDisplay = (typeof VALUES_FORECAST_DISPLAY)[number];
+export type S2DParamForecastDisplay = (typeof VALUES_FORECAST_DISPLAY)[number][0];
 
-const DEFAULT_VALUE_FORECAST_DISPLAY = VALUES_FORECAST_DISPLAY[0];
+const DEFAULT_VALUE_FORECAST_DISPLAY = VALUES_FORECAST_DISPLAY[0][0];
 
 // -----
 
@@ -119,23 +123,8 @@ const slice = createSlice({
 	name: 'seasonal-decadal',
 	initialState,
 	reducers: {
-		setSearchQuery: (state, action) => {
-			state.searchQuery = action.payload;
-		},
 		clearSearchQuery: (state) => {
 			state.searchQuery = '';
-		},
-		setVar: (state, action) => {
-			state.var = action.payload;
-		},
-		setFreq: (state, action) => {
-			state.freq = action.payload;
-		},
-		setForecastType: (state, action) => {
-			state.forecastType = action.payload;
-		},
-		setForecastDisplay: (state, action) => {
-			state.forecastDisplay = action.payload;
 		},
 		resetSeasonalDecadal: (state) => {
 			state.searchQuery = '';
@@ -144,10 +133,47 @@ const slice = createSlice({
 			state.forecastType = DEFAULT_VALUE_FORECAST_TYPE;
 			state.forecastDisplay = DEFAULT_VALUE_FORECAST_DISPLAY;
 		},
+		setForecastDisplay: (state, action) => {
+			state.forecastDisplay = action.payload;
+		},
+		setForecastType: (state, action) => {
+			state.forecastType = action.payload;
+		},
+		setFreq: (state, action) => {
+			state.freq = action.payload;
+		},
+		setSearchQuery: (state, action) => {
+			state.searchQuery = action.payload;
+		},
+		setVar: (state, action) => {
+			state.var = action.payload;
+		},
 	},
 });
 
-const seasonalDecadalReducer = slice.reducer;
+export type ReducerForSeasonalDecadalState = Reducer<SeasonalDecadalStateInterface>
+
+const seasonalDecadalReducer = slice.reducer as ReducerForSeasonalDecadalState;
+
+const {
+	clearSearchQuery,
+	resetSeasonalDecadal,
+	setForecastDisplay,
+	setForecastType,
+	setFreq,
+	setSearchQuery,
+	setVar,
+} = slice.actions
+
+export const seasonalDecadalActions = {
+	clearSearchQuery,
+	resetSeasonalDecadal,
+	setForecastDisplay,
+	setForecastType,
+	setFreq,
+	setSearchQuery,
+	setVar,
+}
 
 export {
 	seasonalDecadalReducer,

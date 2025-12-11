9911ddc54936dfbb34c59daca1ab6eb6c06ad708
Renoir Boulanger <contribs@renoirboulanger.com>
2025-12-11 13:16:20 -0500

    WIP: RM console.log getProbabilityColour

--- apps/src/components/map-layers/s2d-variable-values.tsx
+++ apps/src/components/map-layers/s2d-variable-values.tsx
@@ -219,16 +219,6 @@ export const getProbabilityColour = (
 
 	const colourIndex = findCeilingIndex(colorMap.quantities, queryQuantity);
 
-	if (outcome === 0) {
-		console.log('getProbabilityColour 1\n', {
-			percentage,
-			queryQuantity,
-			queryQuantity2: queryQuantity + CLIM_1234_ADJUSTMENT_FACTOR,
-			colourIndex,
-			colourBeforeChecks: colours[colourIndex],
-		});
-	}
-
 	// If the percentage/outcome is bigger than the highest value
 	if (colourIndex === -1) {
 		return defaultColor;
@@ -259,12 +249,6 @@ export const getProbabilityColour = (
 				break;
 			}
 		}
-	}
-
-	if (outcome === 0) {
-		console.log('getProbabilityColour 2\n', {
-			finalColour: colour,
-		});
 	}
 
 	return colour;

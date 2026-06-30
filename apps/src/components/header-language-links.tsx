b5ee0dbb9aa17de75029f9f0dad7856794abde15
Renoir Boulanger <contribs@renoirboulanger.com>
2026-06-30 16:58:12 -0400

    CLIM-1409: Atom3 URL building to equivalent destination

--- apps/src/components/header-language-links.tsx
+++ apps/src/components/header-language-links.tsx
@@ -1,17 +1,50 @@
 import React from 'react';
 
+import { useLocale } from '@/hooks/use-locale';
+import { type Locale } from '@/types/types';
 import { cn } from '@/lib/utils';
 
 interface HeaderLanguageLinksProps {
 	className?: string;
 }
 
+interface ListItemElementProps {
+	locale: Locale;
+}
+
+const URL_ORDERED_SET = new Set([
+	'/maps/;en',
+	'/cartes/;fr',
+	'/download/;en',
+	'/telechargement/;fr',
+]);
+
+const createHashMapForApp = (pathName: string) =>
+	[...URL_ORDERED_SET]
+		.filter((i) => i.split(';')[0] === pathName)
+		.map((i) => [i.split(';')[1], i.split(';')[0]]);
+
+const URL_HASHMAP_BASEURL = new Map<Locale, string>([
+	['fr', 'https://www.DonneesClimatiques.ca'],
+	['en', 'https://www.ClimateData.ca'],
+]);
+
+const URL_HASHMAP_PATHS_MAPS = new Map<Locale, string>([
+	['fr', '/cartes/'],
+	['en', '/maps/'],
+]);
+
+const URL_HASHMAP_PATHS_DOWNLOAD = new Map<Locale, string>([
+	['fr', '/telechargement/'],
+	['en', '/download/'],
+]);
+
 const HeaderLanguageLinks = (
 	props: HeaderLanguageLinksProps
 ): React.ReactNode => {
-	const {
-		className,
-	} = props;
+	const { className } = props;
+
+	const { locale } = useLocale();
 
 	/**
 	 * IDEA: since we can know the current language.
@@ -29,6 +62,47 @@ const HeaderLanguageLinks = (
 			: [];
 	};
 
+	const currentAppPathname = (() => {
+		const url = new URL(window.location.href);
+		const current = url.pathname.replace('/', '');
+		return current;
+	})();
+
+	const ListItemElement = (props: ListItemElementProps): React.ReactElement => {
+		const { locale } = props;
+		const alternateLangCode = locale === 'fr' ? 'en' : 'fr';
+		const href = URL_HASHMAP_BASEURL.get(locale) as string;
+		// if (currentAppPathname === '/') const pathName = URL_HASHMAP_BASEURL;
+		console.log(`<ListItemElement locale="${locale}">`, { locale, currentLangCode, alternateLangCode });
+
+		return (
+			<>
+				<li
+					className={cn(
+						'hover:text-brand-red',
+						getClassNamesForLangCode(locale)
+					)}
+				>
+					{locale !== alternateLangCode ? (
+						<span>{alternateLangCode}</span>
+					) : (
+						<a href={href + '?yo=dog'}>{alternateLangCode}</a>
+					)}
+				</li>
+			</>
+		);
+	};
+
+	const whatever = createHashMapForApp(currentAppPathname);
+
+	console.log('HeaderLanguageLinks', {
+		currentLangCode,
+		locale,
+		alternateLangCode,
+		currentAppPathname,
+		whatever,
+	});
+
 	return (
 		<>
 			<ul
@@ -38,19 +112,14 @@ const HeaderLanguageLinks = (
 				)}
 				data-lang-code={currentLangCode}
 			>
+				<ListItemElement locale={locale} />
 				<li
-					className={cn(
-						'hover:text-brand-red',
-						getClassNamesForLangCode('en')
-					)}
+					className={cn('hover:text-brand-red', getClassNamesForLangCode('en'))}
 				>
 					<a href="https://www.ClimateData.ca/">en</a>
 				</li>
 				<li
-					className={cn(
-						'hover:text-brand-red',
-						getClassNamesForLangCode('fr')
-					)}
+					className={cn('hover:text-brand-red', getClassNamesForLangCode('fr'))}
 				>
 					<a href="https://www.DonneesClimatiques.ca/">fr</a>
 				</li>

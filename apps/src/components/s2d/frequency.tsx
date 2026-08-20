b0ed4093b82edba1edc08bc1487865da23532b81
Renoir Boulanger <contribs@renoirboulanger.com>
2026-08-20 14:46:08 -0400

    CLIM-1447: Atom25 S2DFrequencyFieldDropdown in French, the DECADAL in parentheses "Décénnale (5 ans; mai-sep)" REVERT RHETORICAL EXAMPLE

--- apps/src/components/s2d/frequency.tsx
+++ apps/src/components/s2d/frequency.tsx
@@ -75,14 +75,6 @@ export const S2DFrequencyFieldDropdown = (
 		...restProps,
 	};
 
-	// BEGIN: We probably DO NOT WANT THIS (no we don't!)
-	for (const option of FrequencyField.options) {
-		if (/Annuelle/.test(option.label)) {
-			option.label = option.label.replace('Annuelle', 'annuelle');
-		}
-	}
-	// END: We probably DO NOT WANT THIS
-
 	return (
 		<Dropdown<S2DFrequencyType | string>
 			key={FrequencyField.key}
